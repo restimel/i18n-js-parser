a670ece93b89de9bbbd0c3e6d66f6d5437c175f9
restimel <restimel@gmail.com>
2002-01-01 02:44:15 +0100

    parse translations object

--- modules/parser.js
+++ modules/parser.js
@@ -40,15 +40,19 @@ Parser.prototype.init = function() {
 	callerNames = '(?:' + config.parser.keys.map(tools.toRegExp).join('|') + ')';
 
 	/* match strings like "i18n('foo' + 'bar')" */
-	matcher = '(?:^|\\W)' + callerNames + '\\(((?:\\s*([\'"])(?:\\\\.|.)*?\\2\\s*(?:\\+(?=\\s*[\'"]))?)+)\\s*[,\\)]';
+	matcher = '(?:^|\\W)' + callerNames + '\\s*\\(((?:\\s*([\'"])(?:\\\\.|.)*?\\2\\s*(?:\\+(?=\\s*[\'"]))?)+)\\s*[,\\)]';
 	this.simpleCall = new RegExp(matcher, 'g');
 
 	/* match strings like "i18n.c('context' + 'ctx', 'foo' + 'bar')" */
 	/* match strings like "i18n.context('context' + 'ctx', 'foo' + 'bar')" */
-	matcher = '(?:^|\\W)' + callerNames + '\.c(?:ontext)?\\(' +
+	matcher = '(?:^|\\W)' + callerNames + '\.c(?:ontext)?\\s*\\(' +
 			  '((?:\\s*([\'"])(?:\\\\.|.)*?\\2\\s*(?:\\+(?=\\s*[\'"]))?)+)\\s*,' +
 			  '((?:\\s*([\'"])(?:\\\\.|.)*?\\4\\s*(?:\\+(?=\\s*[\'"]))?)+)\\s*[,\\)]';
 	this.contextCall = new RegExp(matcher, 'g');
+
+	/* match strings like "i18n({" */
+	matcher = '(?:^|\\W)' + callerNames + '\\s*\\(\\s*\\{';
+	this.attributesCall = new RegExp(matcher, 'g');
 };
 
 Parser.prototype.findItem = function(key, context) {
@@ -115,7 +119,7 @@ Parser.prototype.parseDone = function() {
 }
 
 Parser.prototype.parseFile = function(path, content) {
-	var parse, data, ctx;
+	var parse, data, ctx, idx, i;
 
 	if (this.files.indexOf(path) !== -1) {
 		return;
@@ -140,8 +144,96 @@ Parser.prototype.parseFile = function(path, content) {
 		this.addItem(data, ctx, path);
 	}
 
+	idx = 0;
+	while ((i = content.substr(idx).search(this.attributesCall)) !== -1) {
+		idx += i;
+		this.addAttributesItem(content, idx, path);
+		// to avoid searching on same item
+		idx += 5;
+	}
+
 	this.countFile++;
 }
+
+Parser.prototype.addAttributesItem = function(content, idx, path) {
+	var startIdx, endIdx, countOpen, chr, i, contentLength, i18nAttributes,
+		data, ctx;
+
+	/* extract object */
+	function searchChar(str, idx) {
+		var chr;
+		while (chr !== str && idx < contentLength) {
+			idx++;
+			chr = content.charAt(idx);
+			if (chr === '\\') {
+				idx++;
+			}
+		}
+		return idx;
+	}
+
+	startIdx = content.indexOf('{', idx);
+	idx = startIdx + 1;
+	countOpen = 1;
+	contentLength = content.length;
+
+	while (!endIdx && idx < contentLength) {
+		chr = content.charAt(idx);
+		switch(chr) {
+			case '{': countOpen++; break;
+			case '}':
+				countOpen--;
+				if (!countOpen) {
+					endIdx = idx;
+				}
+				break;
+			case '\'': idx = searchChar('\'', idx); break;
+			case '"': idx = searchChar('"', idx); break;
+			case '/':
+				switch(content.charAt(idx+1)) {
+					case '/': i = content.substr(idx).search(/[\r\n]/); idx += i; break;
+					case '*': i = content.indexOf('*/', idx + 2); idx = i; break;
+				}
+				break;
+		}
+
+		if (idx === -1 || i === -1) {
+			logger.log('cannot extract translation object at char ' + startIdx);
+			return;
+		}
+
+		idx++;
+	}
+
+	if (idx >= contentLength) {
+		logger.log('Translation object at char ' + startIdx + 'seems unclosed');
+		return;
+	}
+
+	i18nAttributes = content.substring(startIdx, endIdx + 1);
+
+	try {
+		eval('i18nAttributes = ' + i18nAttributes + ';');
+	} catch(e) {
+		if (i18nAttributes.length < 45) {
+			logger.log('Cannot parse translation object at char ' + startIdx + ' (' + i18nAttributes + ')');
+		} else {
+			logger.log('Cannot parse translation object at char ' + startIdx + '. It starts with "' + i18nAttributes.substr(0, 20) + '..." and ends with "...' + i18nAttributes.substr(-20) + '"');
+		}
+		return;
+	}
+
+	/* analyse object */
+	if (i18nAttributes.parse) {
+		return;
+	}
+	data = i18nAttributes.string || i18nAttributes.str;
+	ctx = i18nAttributes.context || i18nAttributes.ctx || i18nAttributes.c;
+
+	if (data) {
+		this.addItem(data, ctx, path);
+	}
+};
 
 Parser.prototype.parse = function() {
 	var exclude = config.path.parser.except;
