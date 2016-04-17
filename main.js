18c88f5a601fa32c82c79c5c43ce8b675e6fec11
Restimel <restimel@gmail.com>
2016-04-17 21:33:54 +0200

    Allow to change port number

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ var hasParser = true;
 
 var version = '1.0.0';
 
+/* port number */
+var serverPort = 8000;
+var configurationPath = '';
+
 /**
  * the main entry point of the program
  * @param {String[]} argv list of arguments given my STDIN
@@ -24,23 +28,58 @@ var version = '1.0.0';
  * node main.js [path to configuration.js]
  */
 function main(argv) {
-    if (['-v', '--version'].indexOf(argv[2]) !== -1) {
-        console.log('i18n-js-parser v' + version);
-        process.exit(0);
-    }
+    params(argv);
 
-    configuration.readConfig(argv[2]);
+    configuration.readConfig(configurationPath);
     eventEmitter = new events.EventEmitter();
 
     eventEmitter.once('ready', startProcess);
     initialization.init(eventEmitter);
 }
 
+function params(argv) {
+    var args = argv.slice(2);
+    var arg, value;
+
+    while (args.length) {
+        arg = args.shift();
+
+        if (['-v', '--version'].indexOf(arg) !== -1) {
+            console.log('i18n-js-parser v' + version);
+            process.exit(0);
+        }
+
+        if (arg.indexOf('--port=') === 0) {
+            changePort(arg.slice(7));
+            continue;
+        }
+
+        if (['-p', '--port'].indexOf(arg) !== -1) {
+            changePort(args.shift());
+        }
+
+        // In other cases it should be the path of configuration file
+        if (arg.indexOf('-') !== 0) {
+            configurationPath = arg;
+        }
+    }
+}
+
+function changePort(port) {
+    var portNb = Number(port);
+    if (isNaN(portNb) || portNb > 65535 || portNb < 0 || portNb%1 !== 0) {
+        console.error('Port number "%s" is invalid. Please enter a valid port number.', port);
+        process.exit(1);
+    }
+
+    serverPort = portNb;
+}
+
 function startProcess() {
     parser = new Parser(eventEmitter);
     adapter = new Adapter(eventEmitter);
 
-    web.server(eventEmitter, 8000);
+    web.server(eventEmitter, serverPort);
     output.writer(eventEmitter);
 
     eventEmitter.addListener('parseFiles', parseFiles);
